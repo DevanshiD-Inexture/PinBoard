d70aba58f985857ec897d7c2300859d246a5203b
Devanshi Desai <devanshid.inexture@gmail.com>
2020-06-23 10:20:57 +0530

    feat: added collection update and delete feature

--- collection/urls.py
+++ collection/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import CollectionCreateView, CollectionDetailView
+from .views import (CollectionCreateView, 
+                    CollectionDetailView, 
+                    UserCollectionView,
+                    CollectionDeleteView,
+                    CollectionUpdateView,
+                    CollectionListView,
+                    )
 from . import views
 
 urlpatterns = [
-    path('collection/<int:pk>/', CollectionDetailView.as_view(), name = 'collection-detail'),
-    path('collection/create/', CollectionCreateView.as_view(), name = 'create-collection'),
+    path('', CollectionListView.as_view(), name='home'),
+    path('collections/<str:username>/', UserCollectionView.as_view(), name='user-collections'),
+    path('collection/<int:pk>/', CollectionDetailView.as_view(), name='collection-detail'),
+    path('collection/create/', CollectionCreateView.as_view(), name='create-collection'),
+    path('collection/<int:pk>/update/', CollectionUpdateView.as_view(), name = 'collection-update'),
+	path('collection/<int:pk>/delete/', CollectionDeleteView.as_view(), name = 'collection-delete'),
 ]
